44ddb9e2b746cf9fa98a2df16cbc2c6df5180892
vikash verma <vkdinventor@gmail.com>
2017-06-14 19:02:42 +0530

    AppClass added

--- app/src/main/java/com/vkdinventor/app/foodrecipes/LoginActivity.java
+++ app/src/main/java/com/vkdinventor/app/foodrecipes/LoginActivity.java
@@ -1,13 +1,57 @@
 package com.vkdinventor.app.foodrecipes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.loginContainer)
+    ConstraintLayout loginContainer;
+    @BindView(R.id.loginButton)
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+
+    @OnClick(R.id.loginButton)
+    public void onViewClicked() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(LoginActivity.this, "login Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, "login error :"+error, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
